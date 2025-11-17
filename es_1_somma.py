f36252e463fbfcaf7d4d4a6594b313e8ec8d7ca9
antoniolarosa83-creator <antoniolarosa83-creator@users.noreply.github.com>
2025-11-17 22:54:06 +0100

    modified:   es_1_somma.py

--- es_1_somma.py
+++ es_1_somma.py
@@ -10,3 +10,5 @@ quoziente = numero1 / numero2
 print("Il quoziente tra", numero1, "e", numero2, "è :", quoziente)
 resto = numero1 % numero2
 print("Il resto della divisione tra", numero1, "e", numero2, "è:", resto)   
+potenza = numero1 ** numero2
+print("La potenza di", numero1, "elevato a", numero2, "è:", potenza)        
