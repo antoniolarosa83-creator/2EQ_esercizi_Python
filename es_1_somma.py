5daf885cd7a7ac8e678afc5dd6ea182f1b8fec22
antoniolarosa83-creator <antoniolarosa83-creator@users.noreply.github.com>
2025-11-17 22:42:34 +0100

    Add: esercizio 1 - operazioni matematiche base (somma, differenza, prodotto, quoziente, resto)

--- es_1_somma.py
+++ es_1_somma.py
@@ -2,3 +2,11 @@ numero1 = 10
 numero2 = 18
 somma = numero1 + numero2
 print("La somma di", numero1, "e", numero2, "è:", somma)
+differenza = numero1 - numero2
+print("La differenza tra", numero1, "e", numero2, "è:", differenza)
+prodotto = numero1 * numero2
+print("Il prodotto tra", numero1, "e", numero2, "è:", prodotto)
+quoziente = numero1 / numero2
+print("Il quoziente tra", numero1, "e", numero2, "è :", quoziente)
+resto = numero1 % numero2
+print("Il resto della divisione tra", numero1, "e", numero2, "è:", resto)   
