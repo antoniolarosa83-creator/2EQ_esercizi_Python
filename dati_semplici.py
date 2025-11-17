ccca6a27704490b89b2cfe05d468b0a2be87a71e
antoniolarosa83-creator <antoniolarosa83-creator@users.noreply.github.com>
2025-11-17 23:33:11 +0100

    modified:   dati_semplici.py

--- dati_semplici.py
+++ dati_semplici.py
@@ -1,4 +1,13 @@
 piove=True
 soleggiato=False
 not piove and soleggiato
-print("Oggi è una bella giornata per uscire!") 
+if piove and not soleggiato:
+    print("Oggi è una giornata orrenda per uscire!")
+print ("la variabile piove vale:", piove)
+print ("la variabile soleggiato vale:", soleggiato)
+piove=False
+soleggiato=True
+if not piove and soleggiato:
+    print("Oggi è una bellissima giornata per uscire!")
+print ("la variabile piove vale:", piove)
+print ("la variabile soleggiato vale:", soleggiato)
